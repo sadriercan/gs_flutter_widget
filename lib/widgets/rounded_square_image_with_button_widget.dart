ffdfe3b759679ede81bf76bc486c49d34f49c9f2
Sadri ERCAN <39842101+sadriercan@users.noreply.github.com>
2020-04-14 19:43:01 +0300

    Update rounded_square_image_with_button_widget.dart

--- lib/widgets/rounded_square_image_with_button_widget.dart
+++ lib/widgets/rounded_square_image_with_button_widget.dart
@@ -46,7 +46,7 @@ class RoundedSquareImageWithButtonWidget {
                   borderRadius: new BorderRadius.circular(10.0),
                 ),
                 child: Text(
-                  'LIHAT DETAIL',
+                  'İndir',
                   textAlign: TextAlign.center,
                   style: GSStyle.smallTextButtonStyle,
                 ),
