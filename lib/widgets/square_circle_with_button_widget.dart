e0b185affbf9beb8aa942060fae9a080195c1d60
Sadri ERCAN <39842101+sadriercan@users.noreply.github.com>
2020-04-14 20:18:21 +0300

    Update square_circle_with_button_widget.dart

--- lib/widgets/square_circle_with_button_widget.dart
+++ lib/widgets/square_circle_with_button_widget.dart
@@ -53,7 +53,6 @@ class SquareCircleWithButtonWidget {
                   borderRadius: new BorderRadius.circular(10.0),
                 ),
                 child: Text(
-                  -'SHOW',
                   +'Detaylar'
                   textAlign: TextAlign.center,
                   style: GSStyle.smallTextButtonStyle,
